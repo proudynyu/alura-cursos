80b2eb50994cc8c70e1acf0dc5d515d953a909b0
Igor Becker <igor.becker@acct.global>
2021-09-15 22:57:03 -0300

    adding classes to extends Account class

--- oop-javascript/src/Account.ts
+++ oop-javascript/src/Account.ts
@@ -1,18 +1,45 @@
 import { Client } from "./Client";
 
 export class Account {
-  constructor(public accountValue: number, public client: Client) {}
+  constructor(
+    private _accountValue: number = 0, 
+    private _client: Client,
+    private _agency: string,
+    private _tax: number = 0
+  ) {
+  }
+
+  set client(newClient) {
+    if (newClient instanceof Client) {
+      this._client = newClient;
+    }
+  }
+
+  set accountValue(value: number) {
+    this._accountValue = value;
+  }
+
+  get client() {
+    return this._client;
+  }
+
+  get accountValue() {
+    return this._accountValue;
+  }
 
   deposit(value: number) {
-    return (this.accountValue = this.accountValue - value);
+    return (this._accountValue = this._accountValue + value);
   }
 
   withdraw(value: number) {
-    return (this.accountValue = this.accountValue + value);
+    const tax = this._tax * value
+    if (this._accountValue >= (value + tax)){
+      return (this._accountValue = this._accountValue - value - tax);
+    }
   }
 
   transfer(value: number, client: Account) {
-    if (client instanceof Account && this.accountValue > value) {
+    if (client instanceof Account && this._accountValue > value) {
       client.accountValue = client.accountValue + value;
     }
   }
